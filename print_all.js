da9f8a5df04cade0f23d8cd7971abc0290a772b2
VantorreWannes <VantorreWannes@gmail.com>
2024-06-18 18:31:42 +0200

    Implement script for saving of important data. #8
    - Implement js functions for saving and loading
     flag phrases and content categories.
    
    [Issue: #8]

--- print_all.js
+++ print_all.js
@@ -1,9 +1,45 @@
-// This script would be injected into every page the user visits
-
-function PrintAllText() {
-    const text = document.body.innerHTML;
-    console.log(text);
+async function fetchJsonFile(path) {
+    const response = await fetch(browser.runtime.getURL(path));
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const jsonData = await response.json();
+    return jsonData;
 }
 
-// Call the function on page load
-PrintAllText();
+async function saveFlagPhrases(localStorageKey) {
+    const flagPhrases = await fetchJsonFile('data/data.json');
+    localStorage.setItem(localStorageKey, JSON.stringify(flagPhrases));
+}
+
+async function loadFlagPhrases() {
+    const localStorageKey = "flagPhrases";
+    if (!localStorage.getItem(localStorageKey))  {
+        saveFlagPhrases(localStorageKey);
+    }
+    const flagPhrases = JSON.parse(localStorage.getItem(localStorageKey));
+    return flagPhrases;
+}
+
+async function saveContentCategories(localStorageKey) {
+    const contentCategories = await fetchJsonFile("data/categories.json");
+    localStorage.setItem(localStorageKey, JSON.stringify(contentCategories));
+}
+
+async function loadContentCategories() {
+    const localStorageKey = "contentCategories";
+    if (!localStorage.getItem(localStorageKey))  {
+        saveContentCategories(localStorageKey);
+    }
+    const contentCategories = JSON.parse(localStorage.getItem(localStorageKey));
+    return contentCategories;
+}
+
+async function printAll() {
+    const flagPhrases = await loadFlagPhrases();
+    const contentCategories = await loadContentCategories();
+    console.log(flagPhrases);
+    console.log(contentCategories);
+}
+
+printAll();
