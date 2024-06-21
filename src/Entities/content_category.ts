b6ba621784379a724333af3371b7f30e713a0db0
VantorreWannes <VantorreWannes@gmail.com>
2024-06-21 11:47:05 +0200

    Implement required ContentCategory TS class. #14
    - Implement ContentCategory class with fitting properties.
    
    [Issue: #14]

---
+++ src/Entities/content_category.ts
@@ -0,0 +1,8 @@
+class ContentCategory {
+    name: string
+    description: string
+    constructor(name: string, description: string) {
+        this.name = name
+        this.description = description
+    }
+}
