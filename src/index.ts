1456ff1868dc6bcbed67e5ccbd27fea349c533fe
VantorreWannes <VantorreWannes@gmail.com>
2024-06-22 16:24:21 +0200

    Bring index.ts up to date.
    - Refactor index.ts to take advantage of the new tools.
    
    [Issue: #14]

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
-import { IStoredItem, LocallyStoredItem } from "./Entities/stored_item";
+import { ContentCategory } from "./Entities/content_category";
+import { SearchService } from "./Helpers/search";
 import browser from "webextension-polyfill";
+import { LocallyStoredItem } from "./Helpers/stored_item";
 
 async function main() {
-    const phrases_file_path = browser.runtime.getURL("dist/data/flag_phrases.json");
-    const categories = browser.runtime.getURL("dist/data/content_categories.json");
-    const phrases_stored = new LocallyStoredItem("flag-phrases", phrases_file_path);
-    const categories_stored = new LocallyStoredItem<Array<string>>("content-categories", categories);
-    await printStoredItem(phrases_stored);
-    await printStoredItem(categories_stored);
+    const content_categories_path = browser.runtime.getURL("dist/data/content_categories.json");
+    const stored_content_categories = new LocallyStoredItem<ContentCategory[]>("content-categories", content_categories_path);
+    const content_categories = await stored_content_categories.get();
+    const occurences = getOccurences(document.body.textContent, content_categories);
+    console.log(occurences);
 }
 
-async function printStoredItem<T>(stored: IStoredItem<T>) {
-    const phrases = await stored.get();
-    console.log(phrases);
-} 
+function getOccurences(text: string, content_categories: ContentCategory[]): { category: ContentCategory, found_phrases: string[] }[] {
+    const words: string[] = text.match(/\b[a-zA-Z]+\b/g);
+    const search_service = new SearchService<string>(words);
+    const occurences = content_categories.map(category => {
+        const found_phrases = search_service.findAllOccurences((category.flag_phrases ?? []));
+        if (found_phrases.length > 0) {
+            return { category, found_phrases };
+        }
+    });
+    return occurences.filter(occurence => occurence != undefined);
+}
 
 main();
-    
